ec8d7e5520f5a62038c713b0ab7e37d670bcd420
Tayyaba Abbas Arain <araintayyabaabbas@gmail.com>
2026-08-17 19:20:17 +0500

    Refactor Gemini integration into function

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
 print("Welcome to Hussaini AI")
 
-api_key = os.environ.get("OPENAI_API_KEY")
 gemini_key = os.environ.get("GEMINI_API_KEY")
-print("Python sees key:", api_key is not None)
 print("Python sees Gemini key: ", gemini_key is not None)
-
 
 from google import genai
 client= genai.Client()  #object of class → genai.Client
 
-response = client.models.generate_content(
+def ask_gemini(question):
+    response = client.models.generate_content(
     model = "gemini-flash-lite-latest", 
-    contents="what is AI in roman urdu?"
+    contents=question
     )
-print(response.text)
+    return response.text
+
+print("How can I help you today?")
+question=input()
+
+answer = ask_gemini(question)
+print(answer)
